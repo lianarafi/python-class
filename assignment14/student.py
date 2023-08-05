1afc1a9e0201aa58deb432e0bd058de0d18855b8
lianarafi <137832350+lianarafi@users.noreply.github.com>
2023-08-06 00:48:26 +0330

    Add files via upload

---
+++ assignment14/student.py
@@ -0,0 +1,14 @@
+class student:
+    name=None
+    age=None
+    gender=None
+    hair=None
+    height=None
+
+    def homwork():
+        pass
+    def best_lesson():
+        pass
+    def scor():
+        pass
+    
